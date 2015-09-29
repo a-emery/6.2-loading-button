f77e5c17a7f6860d8b23a978df13b4217f7cdced
Aaron Emery <aemery14@gmail.com>
2015-09-29 12:53:17 -0400

    Initial commit. Installed npm, bower, gulpfile, backbone

---
+++ .tmp/scripts/app.js
@@ -0,0 +1,9 @@
+require.register("main", function(exports, require, module){
+  'use strict';
+
+$(document).ready(function () {
+  // prepend the contents of `app/templates/application.hbs` into `body`
+  $('#container').append(JST.application());
+});
+  
+});
